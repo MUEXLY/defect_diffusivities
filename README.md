9b8d64119e0af99eb2203d40e8f119bf2737fad6
Jacob Jeffries <jwjeffr@node0035.palmetto.clemson.edu>
2024-10-14 21:54:34 -0400

    real stuff

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # defect_diffusivities
+
+## Clone directory and create environment
+
+```bash
+git clone https://github.com/MUEXLY/defect_diffusivities
+python -m venv venv
+venv source/bin/activate
+pip install -r requirements.txt
+```
+
+## Run LAMMPS files
+
+First, grab any LAMMPS executable. My LAMMPS has the tabgab stuff, so soft-link to that executable:
+
+```bash
+cd lammps_data/
+ln -s /home/jwjeffr/software_slurm/lammps-2Aug2023/build/lmp_gpu lmp_gpu
+```
+
+Two input files `interstitial.in` and `vacancy.in` are in `lammps_data/`. To run these for CrTaVW, replace the lines:
+
+```
+# force field
+pair_style eam/alloy
+pair_coeff * * FeCr.eam.alloy Fe Cr
+
+# composition
+set group all type 1
+set type 1 type/ratio 2 0.07 749847
+```
+
+with:
+
+```
+# force field
+mass            1 51.996
+mass            2 180.9479
+mass            3 50.9415
+mass            4 183.84
+
+pair_style hybrid/overlay eam/fs tabgap
+pair_coeff * * eam/fs Cr-Ta-V-W.eam.fs Cr Ta V W
+pair_coeff * * tabgap Cr-Ta-V-W.tabgap Cr Ta V W no yes
+
+# composition
+set type 1 type/ratio 2 0.25 123456
+set type 1 type/ratio 3 0.33333333334 123456
+set type 1 type/ratio 4 0.5 123456
+```
+
+and submit both slurm scripts `interstitial.slurm` and `vacancy.slurm`. The example scripts are for FeCr with 7 at. % Cr. The CrTaVW runs seem to be very slow for some reason.
+
+## Add point defects
+
+In the environment, there will be an executable named `add-pd`, which adds the point defect to the MD run:
+
+```bash
+add-pd lammps_data/interstitial.dump lammps_data/interstitial_with_pd.dump
+add-pd lammps_data/vacancy.dump lammps_data/vacancy_with_pd.dump
+```
+
+The program can also read in and write to files zipped with `gzip`.
+
+## Get MSD curves
+
+Run `analysis.py` to calculate MSD curves.
---
+++ analysis.py
@@ -0,0 +1,147 @@
+from pathlib import Path
+from typing import Callable
+
+import ovito
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+def cross_corr(x, y):
+    """
+    Calculates cross-correlation function of x and y using the 
+    fast Fourier transform.
+    :param x: array[float], data set 1
+    :param y: array[float], data set 2
+    :return: cf: array[float], cross-correlation function
+    """
+    N=len(x)
+    F1 = np.fft.fft(x, n=2**(N*2 - 1).bit_length())
+    F2 = np.fft.fft(y, n=2**(N*2 - 1).bit_length())
+    PSD = F1 * F2.conjugate()
+    res = np.fft.ifft(PSD)
+    res= (res[:N]).real   
+    n=N*np.ones(N)-np.arange(0,N)
+    cf = res/n
+    return cf
+
+
+def msd_fft_cross(r, k):
+    """
+    Calculates "MSD" (cross-correlations) using the fast Fourier transform.
+    :param r: array[float], positions of atom type 1 over time
+    :param k: array[float], positions of atom type 2 over time
+    :return: msd: array[float], "MSD" over time
+    """
+    N=len(r)
+    D=np.multiply(r,k).sum(axis=1)
+    D=np.append(D,0)
+    S2=sum([cross_corr(r[:, i], k[:,i]) for i in range(r.shape[1])])
+    S3=sum([cross_corr(k[:, i], r[:,i]) for i in range(k.shape[1])])
+    Q=2*D.sum()
+    S1=np.zeros(N)
+    for m in range(N):
+        Q=Q-D[m-1]-D[N-m]
+        S1[m]=Q/(N-m)
+    msd = S1-S2-S3
+    return msd
+
+
+def total_displacement_modifier(frame: int, data: ovito.data.DataCollection) -> None:
+
+    types = data.particles['Particle Type'][...]
+    per_particle_displacements = data.particles['Displacement'][...]
+
+    # get per-type data and reduce it
+    for type_ in set(types):
+        displacements_of_type = per_particle_displacements[types == type_]
+        data.attributes[f'Total displacement {type_:.0f}'] = np.sum(displacements_of_type, axis=0)
+
+
+def select_n_closest(particle_index: int, n: int) -> Callable:
+
+    def wrapper(frame: int, data: ovito.data.DataCollection) -> None:
+
+        selection = data.particles_.create_property('Selection', data=None)
+        finder = ovito.data.NearestNeighborFinder(n, data)
+
+        for neigh in finder.find(particle_index):
+            selection[neigh.index] = 1
+
+    return wrapper
+
+
+def recenter_modifier(only_selected: bool = False) -> Callable:
+
+    def wrapper(frame: int, data: ovito.data.DataCollection) -> None:
+        
+        if not only_selected:
+            center_of_mass_displacement = np.mean(data.particles["Displacement"][...], axis=0)
+        else:
+            center_of_mass_displacement = np.mean(data.particles["Displacement"][...][data.particles.selection[...] == 1], axis=0)
+        data.particles_.positions_ -= center_of_mass_displacement
+
+    return wrapper
+
+
+def vacancy():
+
+    # timestep in ns
+    timestep = 1.0e-6
+
+    # load in modified run and recenter it
+    vacancy_run = ovito.io.import_file(Path("lammps_data/vacancy_with_pd.dump"))
+    vacancy_run.modifiers.append(ovito.modifiers.CalculateDisplacementsModifier())
+    vacancy_run.modifiers.append(total_displacement_modifier)
+    vacancy_run.modifiers.append(recenter_modifier(only_selected=False))
+
+    # populate displacement and time arrays
+    displacement = np.zeros((vacancy_run.source.num_frames, 3))
+    time = np.zeros(vacancy_run.source.num_frames)
+    for frame in range(vacancy_run.source.num_frames):
+        
+        data = vacancy_run.compute(frame)
+
+        displacement[frame, :] = data.attributes["Total displacement 0"]
+        time[frame] = timestep * data.attributes["Timestep"]
+
+    # plot results
+    msd = msd_fft_cross(displacement, displacement)
+    plt.plot(time, msd)
+    plt.savefig("v.png")
+
+
+def interstitial():
+
+    # timestep in ns
+    timestep = 1.0e-6
+
+    interstitial_run = ovito.io.import_file(Path("lammps_data/interstitial_with_pd.dump"))
+    interstitial_run.modifiers.append(ovito.modifiers.CalculateDisplacementsModifier())
+    interstitial_run.modifiers.append(total_displacement_modifier)
+
+    # need to unselect the dumb-bell particles before recentering
+    # select particles closest to defect and invert selection
+    interstitial_run.modifiers.append(select_n_closest(particle_index=0, n=2))
+    # invert selection and then recenter
+    interstitial_run.modifiers.append(ovito.modifiers.InvertSelectionModifier())
+    interstitial_run.modifiers.append(recenter_modifier(only_selected=True))
+
+    displacement = np.zeros((interstitial_run.source.num_frames, 3))
+    time = np.zeros(interstitial_run.source.num_frames)
+    for frame in range(interstitial_run.source.num_frames):
+        
+        data = interstitial_run.compute(frame)
+        displacement[frame, :] = data.attributes["Total displacement 0"]
+        time[frame] = timestep * data.attributes["Timestep"]
+
+    msd = msd_fft_cross(displacement, displacement)
+    plt.plot(time, msd)
+    plt.savefig("i.png")
+
+
+if __name__ == "__main__":
+
+    mpl.use("Agg")
+    vacancy()
+    interstitial()
